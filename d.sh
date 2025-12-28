2bc214d844473683e07667ff364bf2378975f265
Highlyhotgames <100530584+Highlyhotgames@users.noreply.github.com>
2025-12-28 19:09:18 +0000

    Add additional model downloads to d.sh

--- d.sh
+++ d.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 hf download --local-dir models/txt --max-workers 14 unsloth/Qwen3-1.7B-GGUF Qwen3-1.7B-UD-Q8_K_XL.gguf
+hf download --local-dir models/txt --max-workers 14 unsloth/Qwen3-4B-GGUF Qwen3-4B-UD-Q8_K_XL.gguf
+hf download --local-dir models/txt --max-workers 14 unsloth/Qwen3-4B-128K-GGUF Qwen3-4B-128K-UD-Q8_K_XL.gguf
